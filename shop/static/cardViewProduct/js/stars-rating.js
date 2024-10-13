7b2d2dc5ea8d92af28cb15b06e15dcfb961a3067
Panu.Mac <nktleznin@gmail.com>
2024-10-14 02:23:38 +0300

    add more info product

--- shop/static/cardViewProduct/js/stars-rating.js
+++ shop/static/cardViewProduct/js/stars-rating.js
@@ -1,7 +1,28 @@
-document.querySelectorAll('.rating').forEach(function(ratingElement) {
-    const rating = ratingElement.getAttribute('data-rating'); // Получаем рейтинг
-    if (rating) {
-        ratingElement.style.setProperty('--rating', rating); // Устанавливаем переменную
-        console.log("finish");
+const ratings = document.querySelectorAll('.rating');
+if (ratings.length > 0) {
+    initRatings();
+}
+
+function initRatings() {
+    let ratingActive, ratingValue;
+
+    for (let index = 0; index < ratings.length; index++) {
+        const rating = ratings[index];
+        initRating(rating);
     }
-});
+}
+
+function initRating(rating) {
+    initRatingVars(rating);
+    setRatingActiveWidth();
+}
+
+function initRatingVars(rating) {
+    ratingActive = rating.querySelector('.rating__active'); // querySelector возвращает один элемент
+    ratingValue = rating.querySelector('.rating__value'); // querySelector возвращает один элемент
+}
+
+function setRatingActiveWidth(index = ratingValue.innerHTML) {
+    const ratingActiveWidth = index * 20; // Умножаем на 20, чтобы получить процент (5 звезд = 100%)
+    ratingActive.style.width = `${ratingActiveWidth}%`;
+}
